05f666bf65274c9ff7fd67dca42cb8e759d4ed96
balaji.m <balaji.m@hakunamatata.in>
2024-03-15 11:29:28 +0530

    webhooks api added

---
+++ config/config.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+
+// Extract configuration values from environment variables or use defaults
+const APP_PORT = process.env.APP_PORT || 3000;
+const APP_HOST = process.env.APP_HOST || "localhost";
+const APP_VERSION = "0.0.1";
+const LOGLEVEL = process.env.LOGLEVEL ? process.env.LOGLEVEL : "error";
+
+const config = {
+  app: {
+    app_version: APP_VERSION,
+    host: APP_HOST,
+    port: APP_PORT,
+    logger: { level: LOGLEVEL },
+  },
+  services: {
+    SVC_TENANT_URL: process.env.SVC_TENANT_URL,
+  },
+};
+
+module.exports = config;
--- index.js
+++ index.js
@@ -1,41 +1,22 @@
-const fastify = require('fastify')({
-  logger: process.env.APP_LOGLEVEL || 'debug'
+const routes = require("./lib/routes");
+
+const fastify = require("fastify")({
+  logger: process.env.APP_LOGLEVEL || "debug",
 });
-const PORT = process.env.PORT || 3000;
-const HOST = process.env.APP_HOST || 'localhost'
-const packageJson = require('./package.json')
-fastify.register(require('@fastify/cors'), {
-  origin: '*'
-});
-fastify.get('/webhooks/ping', (req, res) => {
+const PORT = process.env.APP_PORT || 3000;
+const HOST = process.env.APP_HOST || "localhost";
 
-  return { message: 'pong', version: packageJson.version };
-});
+fastify
+  .register(require("@fastify/cors"), {
+    origin: "*",
+  })
+  .register(routes);
 
-fastify.post('/webhooks/zoho/sync_gcp', (req, res) => {
-  // Extract data from request body 
-  const p = req.body;
-  console.log(p, req.params, req.query);
-  return { message: 'Data received successfully', data: p };
-});
-
-fastify.post('/webhooks/zoho/sync_contact', (req, res) => {
-  // Extract data from request body 
-  const p = req.body;
-  console.log(p, req.params, req.query);
-
-  return { message: 'Data received contact successfully', data: p };
-});
-
-fastify.listen({ port: PORT, host: HOST }).then((address) => {
-  console.log(`Server is running on ${address}`);
-}).catch((err) => {
-  console.log('Error starting server:', err);
-});
-
-
-
-
-
-
-
+fastify
+  .listen({ port: PORT, host: HOST })
+  .then((address) => {
+    console.log(`Server is running on ${address}`);
+  })
+  .catch((err) => {
+    console.log("Error starting server:", err);
+  });
